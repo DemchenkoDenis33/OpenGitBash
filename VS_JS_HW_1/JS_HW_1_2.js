33ce134be29409976af1b1b9f4160889c4bf318a
DemchenkoDenis33 <demchenko.denis89@gmail.com>
2022-06-20 19:57:54 +0300

    VS_JS_HW_1

--- VS_JS_HW_1/JS_HW_1_2.js
+++ VS_JS_HW_1/JS_HW_1_2.js
@@ -2,30 +2,29 @@
 // Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
 
 
-let age_1 = 10
 let age_2 = 18
 let age_3 = 60
 
-const checkAge = function (age){
-
-if (typeof age == Number){
-if (age < age_2)
-console.log("You don’t have access cause your age is ” + age_1 + “ It’s less then")
-
-else if (age >= age_2, age_1 < age_3)
-console.log("Welcome !")
-
-else if (age > age_3)
-console.log("Keep calm and look Culture channel")
-
-else 
-console.log("Technical work")
+const checkAge = function (age) {
+   age = Number(age)
+   console.log(age)
+if(age) {
+    if (age < age_2){
+         console.log("You don’t have access cause your age is ”" + age + "“ It’s less then");
+    } else if (age >= age_2 && age < age_3) {
+         console.log("Welcome !");
+    } else if (age > age_3) {
+         console.log("Keep calm and look Culture channel");
+    } else {
+         console.log("Technical work");
+    }
+} else console.log("Error"); 
 }
-else {
-console.log("Error") 
-}
-}
+
 checkAge(17)
 checkAge(18)
+checkAge(60)
 checkAge(61)
 checkAge('qqqaws')
+checkAge('19')
+checkAge('0')
--- VS_JS_HW_1/JS_HW_1_3.js
+++ VS_JS_HW_1/JS_HW_1_3.js
@@ -2,33 +2,28 @@
 // Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
 
 
-let age_1 = 10
 let age_2 = 18
 let age_3 = 60
 
-const checkAge = function (age){
+const checkAge = function (age) {
 
-age = Number(age) 
-
-if (!isNaN(age)) {
-
-if (age < age_2)
-console.log("You don’t have access cause your age is ” + age_1 + “ It’s less then")
-
-else if (age >= age_2, age_1 < age_3)
-console.log("Welcome !")
-
-else if (age > age_3)
-console.log("Keep calm and look Culture channel")
-
-else 
-console.log("Technical work")
-}
-else {
-console.log("Error") 
-}
+if(age && !isNaN (age)) {
+    if (age < age_2){
+         console.log("You don’t have access cause your age is ”" + age + "“ It’s less then");
+    } else if (age >= age_2 && age < age_3) {
+         console.log("Welcome !");
+    } else if (age > age_3) {
+         console.log("Keep calm and look Culture channel");
+    } else {
+         console.log("Technical work");
+    }
+} else console.log("Error"); 
 }
 checkAge(17)
 checkAge(18)
+checkAge(60)
 checkAge(61)
-checkAge('32qqqaws')
+checkAge('qqqaws')
+checkAge('19')
+checkAge('0')
+checkAge('')
